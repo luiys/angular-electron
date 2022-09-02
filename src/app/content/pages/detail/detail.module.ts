51a86483f7640fa89b03d5afd9b90cd65496bc77
Luis Gustavo <Luisgustavofeitoza@gmail.com>
2022-09-02 13:31:42 -0300

    Estrutura de pastas adaptada

--- src/app/content/pages/detail/detail.module.ts
+++ src/app/content/pages/detail/detail.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DetailRoutingModule } from './detail-routing.module';
 
 import { DetailComponent } from './detail.component';
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [DetailComponent],
-  imports: [CommonModule, SharedModule, DetailRoutingModule]
+    declarations: [DetailComponent],
+    imports: [CommonModule, SharedModule, DetailRoutingModule]
 })
-export class DetailModule {}
+export class DetailModule { }
